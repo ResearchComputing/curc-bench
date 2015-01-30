2911199adfb7a5c6a4fdca03782a250333492d12
AaronTHolt <holtat@colorado.edu>
2015-01-30 11:24:07 -0700

    Added more scripts

---
+++ scripts/submit_jobs.py
@@ -0,0 +1,12 @@
+#!/curc/admin/benchmarks/bin/python
+import os
+
+for dirname, dirnames, filenames in os.walk('.'):
+    for filename in filenames:
+        if filename.find("script_") == 0:
+            print filename
+            cmd = "qsub " + filename
+            os.system(cmd)
+            
+            
+
