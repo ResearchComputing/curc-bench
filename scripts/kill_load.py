8340efcd8fd82aad117213e7124b56d5773b04ea
AaronTHolt <holtat@colorado.edu>
2015-02-16 11:02:34 -0700

    Remove 'software' and 'scripts' directories

--- scripts/kill_load.py
+++
@@ -1,4 +0,0 @@
-#!/curc/admin/benchmarks/bin/python
-import os
-
-os.system("pdsh -w node[01-17][01-80] 'killall -u molu8455'")
--- scripts/submit_jobs.py
+++
@@ -1,9 +0,0 @@
-#!/curc/admin/benchmarks/bin/python
-import os
-
-for dirname, dirnames, filenames in os.walk('.'):
-    for filename in filenames:
-        if filename.find("script_") == 0:
-            print filename
-            cmd = "qsub " + filename
-            os.system(cmd)
