c764baa947d49a4b977cd43998bb69e7639330df
AaronTHolt <holtat@colorado.edu>
2015-02-20 11:12:57 -0700

    Add linpack 11.2.2 download to script -remove stream.c from script as it has been tuned -copy the stream.c from old version to lib directory and include it in repository

--- Non_Python_Dependencies.sh
+++ Non_Python_Dependencies.sh
@@ -8,22 +8,12 @@ tar xf osu-micro-benchmarks-3.8-July12.tar
 rm osu-micro-benchmarks-3.8-July12.tar
 mv ./osu-micro-benchmarks-3.8-July12 ./lib/osu-micro-benchmarks-3.8
 
-# cd ./lib/osu-micro-benchmarks-3.8/
-# ./configure
-# make
-# make install
-# cd ..
-
 
 # IOR-2.10.3
 wget http://colocrossing.dl.sourceforge.net/project/ior-sio/IOR%20latest/IOR-2.10.3/IOR-2.10.3.tgz
 tar zxvf IOR-2.10.3.tgz
 rm IOR-2.10.3.tgz
 mv ./IOR ./lib/IOR
-
-# cd ./lib/IOR/
-# make [posix|mpiio|hdf5|ncmpi|all]
-# cd ..
 
 
 #HPL 2.0
@@ -39,8 +29,19 @@ tar zxvf hpcc-1.4.2.tar.gz
 rm hpcc-1.4.2.tar.gz
 mv ./hpcc-1.4.2 ./lib/hpcc-1.4.2
 
+# #Note:The version of stream.c included has numbers that have been changed
+# #stream.c v5.9
+# wget http://www.cs.virginia.edu/stream/FTP/Code/stream.c
+# mkdir ./lib/stream/
+# mv ./stream.c ./lib/stream/stream.c
 
-#stream.c v5.9
-wget http://www.cs.virginia.edu/stream/FTP/Code/stream.c
-mkdir ./lib/stream/
-mv ./stream.c ./lib/stream/stream.c
+
+#Note: The version originally used (10.3.9) is no longer available on the INTEL site
+#This version may or may not produce consitant results (in theory it just adds support
+#for the latest architecture along with some optimizations)
+#Also, you may have to accept a license agreement to download
+#linpack
+wget http://registrationcenter.intel.com/irc_nas/5232/l_lpk_p_11.2.2.010.tgz
+tar zxvf l_lpk_p_11.2.2.010.tgz
+rm l_lpk_p_11.2.2.010.tgz
+mv ./l_lpk_p_11.2.2.010 ./lib/linpack
