93b51e3c9e0b79aa30b2401e2f2e89f271526e6c
AaronTHolt <holtat@colorado.edu>
2015-03-02 15:23:09 -0700

    Formatting

--- src/tests/test_suite.py
+++ src/tests/test_suite.py
@@ -1,39 +1,38 @@
-#Automated test suite for curc-bench
+"""Automated test suite for curc-bench"""
 import unittest
 import re
 
 
-def slurmNodesSetup():
-
-    with open('nodeInfo', 'r') as f:
-    	nodeInfo = f.read()
-    with open('freeNodeList', 'r') as f:
-    	freeNodes = f.read()
-    	freeNodes = freeNodes.splitlines()
-    return nodeInfo, freeNodes
-
-#Trying to get automated tests to run
-def unimportantTest():
-	return 1
+def slurm_setup():
+    """Loads results from old subprocess.Popen implementation"""
+    with open('nodeInfo', 'r') as node_file:
+        node_info = node_file.read()
+    with open('freeNodeList', 'r') as free_node_file:
+        free_nodes = free_node_file.read()
+        free_nodes = free_nodes.splitlines()
+    return node_info, free_nodes
 
 class MyTest(unittest.TestCase):
+    """Test cases for curc-bench"""
     def test(self):
-        self.assertEqual(unimportantTest(), 1)
+        """An unimportant test"""
+        self.assertEqual(1, 1)
 
-    def test_slurmFreeNodes(self):
-    	#test the functionalisty of pyslurm compared to subprocess Popen commands
-    	# nodeInfo is the result of the following command on a login node:
-	    	#scontrol -o show nodes | grep Reason
-	   	# freeNodes is the result of:
-	   		# grep -o -E "NodeName=(node[0-9]+)" nodeInfo > nodelist
-	   		# grep -o -E "(node[0-9]+)" nodelist > freeNodeList
+    def test_free_SLURM_nodes(self):
+        """Test the functionalisty of pyslurm compared to subprocess.Popen commands"""
+        # node_info is the result of the following command on a login node:
+            #scontrol -o show nodes | grep Reason
+        # free_nodes is the result of:
+            # grep -o -E "NodeName=(node[0-9]+)" nodeInfo > nodelist
+            # grep -o -E "(node[0-9]+)" nodelist > freeNodeList
+        node_info, free_nodes = slurm_setup()
+        nodes = re.findall(r'NodeName=(node[0-9]+)', node_info)
+        #Test that the lists of freenodes match
+        self.assertEqual(nodes, free_nodes)
 
-    	nodeInfo, freeNodes = slurmNodesSetup()
-    	nodes = re.findall(r'NodeName=(node[0-9]+)', nodeInfo)
-    	#Test that the lists of freenodes match
-    	self.assertEqual(nodes,freeNodes)
-
-    	#TODO: test pyslurm functionality matches the above functionality
+        #TODO: test that pyslurm functionality matches the above functionality
+        
+        
 
 if __name__ == '__main__':
     unittest.main()
