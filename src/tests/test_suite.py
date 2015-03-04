a54abaceb2866ec8745031c7dbfb5c578762a15d
Aaron Holt <holtat@login02.rc.colorado.edu>
2015-03-04 11:24:31 -0700

    test_free_Slurm_nodes now finds free nodes

--- src/tests/test_suite.py
+++ src/tests/test_suite.py
@@ -5,7 +5,7 @@ import pyslurm
 import os
 import subprocess
 
-from bench.util.hostlist import expand_hostlist
+#from bench.util.hostlist import expand_hostlist
 
 def slurm_setup():
     """Loads results from old subprocess.Popen implementation"""
@@ -37,48 +37,52 @@ class MyTest(unittest.TestCase):
         #TODO: test that pyslurm functionality matches the above functionality
 
         
-        node_list = expand_hostlist("node[01-17][01-80]")
-        p1 = subprocess.Popen(['scontrol', '-o', 'show', 'nodes'], stdout=subprocess.PIPE)
-        p2 = subprocess.Popen(['grep', 'Reason'], stdin=p1.stdout, stdout=subprocess.PIPE)
-        p1.stdout.close()
-        out, err = p2.communicate()
+        #node_list = expand_hostlist("node[01-17][01-80]")
+        #p1 = subprocess.Popen(['scontrol', '-o', 'show', 'nodes'], stdout=subprocess.PIPE)
+        #p2 = subprocess.Popen(['grep', 'Reason'], stdin=p1.stdout, stdout=subprocess.PIPE)
+        #p1.stdout.close()
+        #out, err = p2.communicate()
 
-        not_available_nodes = re.findall(r'NodeName=(node[0-9]+)', out)
+        #not_available_nodes = re.findall(r'NodeName=(node[0-9]+)', out)
 
-        diff_set = set(node_list).difference(set(not_available_nodes))
+        #diff_set = set(node_list).difference(set(not_available_nodes))
         
-        for nn in diff_set:
-            print nn
+        #for nn in diff_set:
+        #    print nn
 
 
 
 
         a = pyslurm.node()
         node_dict = a.get()
-
+        slurm_free_nodes=[]
         #if len(node_dict) > 0:
         ii = 0
         print "-" * 80
         for key, value in node_dict.iteritems():
-            if ii > 3:
-                break
-            ii += 1
-            print "%s :" % (key)
+            #if ii > 30:
+            #    break
+            #ii += 1
+            #print "%s :" % (key)
             for part_key in sorted(value.iterkeys()):
                 #print "\t%-15s : %s" % (part_key, value[part_key])
-                if part_key == 'node_hostname':
-                    print "    node_hostname = ",value[part_key]
-                elif part_key == 'node_state':
-                    print "    node_state = ",value[part_key]
-                elif part_key == 'reason':
-                    print "    reason = ", value[part_key]
+                if part_key == 'node_state':
+                    #print "%s :" % (key), "    node_state = ",value[part_key]
+                    if value[part_key] == 'IDLE':
+                        if key[0:4] == 'node':
+                            if int(key[4:6])>=01 and int(key[4:6])<=17:
+                                if int(key[6:8])>=01 and int(key[6:8])<=80:
+                                    slurm_free_nodes.append(key)
                 #else:
                 #    print "key = ",part_key
-            print "-" * 80
+            #print "-" * 80
 
         #else:
         #    print "No Nodes found !"
-        
+        print ''
+        print "Number of slurm free nodes = ",len(slurm_free_nodes)
+        #for nn in slurm_free_nodes:
+        #    print nn
 
 if __name__ == '__main__':
     unittest.main()
