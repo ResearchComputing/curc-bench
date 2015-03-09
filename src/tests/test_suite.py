09db9134995d21caa87278c424ad275a5a4528e0
AaronTHolt <holtat@colorado.edu>
2015-03-09 10:53:54 -0600

    Modify test_suite again

--- src/tests/test_suite.py
+++ src/tests/test_suite.py
@@ -9,20 +9,6 @@ from create import free_SLURM_nodes
 from create import reservations
 from hostlist import expand_hostlist
 
-#from bench.util.hostlist import expand_hostlist
-
-def slurm_setup():
-    """Loads results from old subprocess.Popen implementation"""
-    with open('nodeInfo', 'r') as node_file:
-        node_info = node_file.read()
-    with open('freeNodeList', 'r') as free_node_file:
-        free_nodes = free_node_file.read()
-        free_nodes = free_nodes.splitlines()
-    with open('all_nodes.txt', 'r') as all_nodes_file:
-        all_nodes = all_nodes_file.read()
-        all_nodes = all_nodes.splitlines()
-
-    return node_info, free_nodes, all_nodes
 
 class MyTest(unittest.TestCase):
     """Test cases for curc-bench"""
@@ -32,18 +18,8 @@ class MyTest(unittest.TestCase):
 
     def test_free_SLURM_nodes(self):
         """Test the functionalisty of pyslurm compared to subprocess.Popen commands"""
-        # node_info is the result of the following command on a login node:
-            #scontrol -o show nodes | grep Reason
-        # free_nodes is the result of:
-            # grep -o -E "NodeName=(node[0-9]+)" nodeInfo > nodelist
-            # grep -o -E "(node[0-9]+)" nodelist > freeNodeList
-        node_info, free_nodes, all_nodes = slurm_setup()
-        nodes = re.findall(r'NodeName=(node[0-9]+)', node_info)
-        #Test that the lists of freenodes match
-        self.assertEqual(nodes, free_nodes)
 
-
-        #TODO: test that pyslurm functionality matches the above functionality
+        #TODO: test that pyslurm functionality matches old functionality
         free_nodes = free_SLURM_nodes("node[01-17][01-80]")
         reserved_nodes = reservations()
 
@@ -51,7 +27,6 @@ class MyTest(unittest.TestCase):
         node_list = []
         for node in diff_set:
             node_list.append(node)
-
 
 
         a = pyslurm.node()
