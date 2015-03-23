688f3245bba79b22ae54a5a59b5d432228b5470c
Jonathon Anderson <jonathon.anderson@colorado.edu>
2015-03-23 11:42:28 -0600

    Clean-up bench.driver
    First pass on a clean-up audit before I start changing
    functionality for new features in bench.create.

--- src/bench/driver.py
+++ src/bench/driver.py
@@ -3,7 +3,6 @@ import os
 import datetime
 import sys
 
-#from create import execute
 import bench.create as create
 import bench.submit as submit
 
@@ -12,7 +11,6 @@ import bench.process as process
 import bench.reserve as reserve
 import bench.nodelist as nodelist
 import bench.add as add
-#import bench.scaling as scaling
 import bench.nodes
 
 import automation
@@ -23,20 +21,16 @@ import shutil
 
 
 def get_args(argv):
-
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(help=None, dest='command')
 
-    # Create
-    create = subparsers.add_parser('create', help='Create the benchmark test scripts.  For options, type bench create -h for help.\n')
+    create = subparsers.add_parser('create', help='Create the benchmark test scripts.')
     create.add_argument('-d','--dir', help='directory', dest='directory')
     create.set_defaults(directory=None)
-    #create.add_argument('-r','--retest', help='retest benchmarks', action='store_true')
     create.add_argument('-l','--nodelist', help='node_list for test')
     create.add_argument('-r','--reservation', help='node_list from reservation')
     create.set_defaults(nodelist=None)
     create.add_argument('-e','--exclude', help='exclude these nodes.  For example, --exclude node[01][01-80]')
-    #create.set_defaults(exclude='node[01][01-80],node[00][01-04],node[02][01-08], node0773')
     create.set_defaults(exclude='node[00][01-04], node0773')
 
     add = subparsers.add_parser('add', help='Add a benchmark test.  For options, type bench add -h for help.\n')
@@ -48,8 +42,7 @@ def get_args(argv):
     add.add_argument('-n','--nodes', help='nodes', action='store_true')
     add.add_argument('-b','--bandwidth', help='bandwidth', action='store_true')
 
-    # Submit
-    sumbit = subparsers.add_parser('submit', help='Submit all the jobs from create to the scheduler. For options, type bench submit -h for help.\n')
+    sumbit = subparsers.add_parser('submit', help='Submit all the jobs from create to the scheduler.')
     sumbit.add_argument('-d','--dir', help='directory', dest='directory')
     sumbit.set_defaults(directory=None)
     sumbit.add_argument('-r','--allrack', help='alltoall rack level', action='store_true')
@@ -58,8 +51,7 @@ def get_args(argv):
     sumbit.add_argument('-n','--nodes', help='nodes', action='store_true')
     sumbit.add_argument('-b','--bandwidth', help='bandwidth', action='store_true')
 
-    # Process
-    process = subparsers.add_parser('process', help='Process the jobs when they are finished. For process options, type bench submit -h for help.\n')
+    process = subparsers.add_parser('process', help='Process the jobs when they are finished.')
     process.add_argument('-d','--dir', help='directory', dest='directory')
     process.set_defaults(directory=None)
     process.add_argument('-r','--allrack', help='alltoall rack level', action='store_true')
@@ -68,8 +60,7 @@ def get_args(argv):
     process.add_argument('-n','--nodes', help='nodes', action='store_true')
     process.add_argument('-b','--bandwidth', help='bandwidth', action='store_true')
 
-    # Reserve
-    reserve = subparsers.add_parser('reserve', help='Create the necessary reservations: For options, type bench reserve -h.\n')
+    reserve = subparsers.add_parser('reserve', help='Create the necessary reservations.')
     reserve.add_argument('-d','--dir', help='directory', dest='directory')
     reserve.set_defaults(directory=None)
     reserve.add_argument('-r','--allrack', help='alltoall rack level', action='store_true')
@@ -78,29 +69,17 @@ def get_args(argv):
     reserve.add_argument('-n','--nodes', help='nodes', action='store_true')
     reserve.add_argument('-b','--bandwidth', help='bandwidth', action='store_true')
 
-    # Showq info
-    showq = subparsers.add_parser('q', help='Checks the status of running jobs.  For additional information, type bench q -v or bench q --verbose.\n')
+    showq = subparsers.add_parser('q', help='Checks the status of running jobs.')
     showq.add_argument('-v','--verbose', help='verbose', action='store_true')
 
-    # Node information
     nodes = subparsers.add_parser('nodes', help='Checks the status of nodes.')
 
-    # Automation
-    automation = subparsers.add_parser('execute', help='An automation of the entire process.  To specify a directory, type bench execute -d <DIR_NAME>.\n')
-    automation.add_argument('-d','--dir', help='directory', dest='directory')
-    automation.set_defaults(directory=None)
-    automation.add_argument('-r','--retest', help='retest benchmarks', action='store_true')
-
-    # Status
-    status = subparsers.add_parser('status', help='Concatentates the log file of the current directory.  To specify a directory, type bench status -d <DIR_NAME>.\n')
+    status = subparsers.add_parser('status', help='Concatentates the log file of the current directory.')
     status.add_argument('-d','--dir', help='directory', dest='directory')
     status.set_defaults(directory=None)
 
-#    scaling = subparsers.add_parser('scaling', help='Add scaling studies.  To specify a directory, type bench status -d <DIR_NAME>.\n')
-#    scaling.add_argument('-d','--dir', help='directory', dest='directory')
-#    scaling.set_defaults(directory=None)
-
     return parser.parse_args(argv)
+
 
 def get_directory(dir_name=None):
 
@@ -140,6 +119,7 @@ def get_new_directory():
     folder_name = os.path.join(directory,str(folder)+"-"+str(index))
     return folder_name
 
+
 class MyFormatter(logging.Formatter):
 
     def format(self, record):
@@ -170,6 +150,7 @@ def get_logger(directory):
 
     return logger
 
+
 def create_directory(directory_name):
 
     if os.path.exists(directory_name):
@@ -177,15 +158,13 @@ def create_directory(directory_name):
 
     os.makedirs(directory_name)
 
-def driver():
 
-    # Get the working directory
+def driver():
     args = get_args(sys.argv[1:])
 
-    # Create the working directory
     directory = None
     try:
-        if not args.directory and (args.command == 'create' or args.command=='execute'):
+        if not args.directory and args.command == 'create':
             directory = get_new_directory()
         elif args.directory:
             directory = get_directory(args.directory)
@@ -194,15 +173,13 @@ def driver():
     except:
         directory = get_directory()
 
-    if args.command == 'create' or args.command == 'execute':
+    if args.command == 'create':
         create_directory(directory)
 
-    # Create the logger
     logger = get_logger(directory)
 
     if args.command == 'create':
         create.execute(directory, args.nodelist, args.exclude, args.reservation)
-
 
     if args.command == 'add':
         add.execute(directory, args)
@@ -216,24 +193,11 @@ def driver():
     if args.command == 'reserve':
         reserve.execute(directory, args)
 
-    # if args.command == 'l':
-        #TODO: run tests on a set of nodes specified by nodelist in filename
-
-    # if args.command == 'createCustom':
-        #TODO: user specified reservation instead of PM
-
     if args.command == 'q':
         showq.execute(args.verbose)
 
-    if args.command == 'execute':
-        #automation.execute(directory, args.retest)
-        logger.error("This function is disabled at the moment")
-
     if args.command == 'status':
         status.execute(directory)
-
-#    if args.command == 'scaling':
-#        scaling.execute(directory)
 
     if args.command == 'nodes':
         nodes.execute()
