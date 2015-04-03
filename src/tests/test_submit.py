cc8ca6e5b0f9f74f9ba0171b34d0d77548beb79b
AaronTHolt <holtat@colorado.edu>
2015-04-03 10:22:19 -0600

    Fix test case in TestSubmit

--- src/tests/test_submit.py
+++ src/tests/test_submit.py
@@ -12,15 +12,32 @@ def fake_subprocess(cmd, shell):
     subprocess.check_output = mock.Mock(return_value=([cmd], shell))
     return subprocess
 
-class TestSubmit (unittest.TestCase):
+class TestSubmit(unittest.TestCase):
+
+	def setUp (self):
+		self.directory = tempfile.mkdtemp()
+		self.folder = tempfile.mkdtemp(dir=self.directory)
+		self.file = tempfile.NamedTemporaryFile(prefix='script_10', dir=self.folder)
+
+	def tearDown (self):
+		shutil.rmtree(self.directory)
 
 	@mock.patch('bench.submit.subprocess.check_output',
 				return_value=fake_subprocess('sbatch script_10', True))
-	def test_submit(self):
-		d = TempDirectory()
-		path = d.write(('folder', 'script_10'), b'text in the script')
+	def test_submit(self, _):
 		index = 0
 		pause = None
 		reservation = None
-		bench.submit.submit(d, d.folder, index, pause, reservation)
-		self.assertEqual(1,1)
+		qos = None
+		account = None
+		new_index = bench.submit.submit(self.directory, self.folder,
+							 			index, pause, reservation, qos, account)
+
+		#Did it find the file?
+		self.assertEqual(1,new_index)
+
+		
+
+
+if __name__ == '__main__':
+	unittest.main()
