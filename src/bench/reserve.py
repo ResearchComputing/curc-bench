892bd7c9f7957cd82067487317abba62738803ef
AaronTHolt <holtat@colorado.edu>
2015-04-08 11:56:38 -0600

    Replace subprocess with pyslurm in bench.reserve Still need to check formatting of dates and account name

--- src/bench/reserve.py
+++ src/bench/reserve.py
@@ -1,13 +1,15 @@
 import os
 import subprocess
 from bench.util import util
+import pyslurm
+import datetime
 
 import logging
 logger = logging.getLogger('Benchmarks')
 
-def create_reservation(data_name,label,directory):
+def create_reservation(data_name, label, directory):
 
-    data = util.read_node_list(data_name,directory)
+    data = util.read_node_list(data_name, directory)
 
     #print data
     try:
@@ -16,13 +18,26 @@ def create_reservation(data_name,label,directory):
             # Adding ^ to fix cnode01.. node01.. issue
             #node_string = "".join(['^%s$,' % n for n in data])
             node_string = "".join(['%s,' % n for n in data])
-            print node_string[:-1]
-            os.environ['NODE_LIST'] = node_string[:-1]
-            #cmd = "mrsvctl -c -h $NODE_LIST -n " + label
-            cmd = 'scontrol create reservation={0} accounts=crcbenchmark flags=overlap starttime=now duration=32-0 nodes=$NODE_LIST'.format(label)
 
-            print cmd
-            cmd_out = subprocess.check_output([cmd] , shell=True)
+            # print node_string[:-1]
+            # os.environ['NODE_LIST'] = node_string[:-1]
+            # #cmd = "mrsvctl -c -h $NODE_LIST -n " + label
+            # cmd = 'scontrol create reservation={0} accounts=crcbenchmark flags=overlap starttime=now duration=32-0 nodes=$NODE_LIST'.format(label)
+            
+            # print cmd
+            # cmd_out = subprocess.check_output([cmd] , shell=True)
+
+            res_dict = pyslurm.create_reservation_dict()
+            res_dict['Accounts'] = 'crcbenchmark'            #Is this the desired account name?
+            res_dict['Flags'] = 'overlap'
+            res_dict['StartTime'] = datetime.datetime.now()  #Check time formatting
+            res_dict['Duration'] = '32-0'                    #Check time formatting
+            res_dict['Name'] = label
+            res_dict['Nodes'] = node_string[:-1]
+
+            resid = pyslurm.slurm_create_reservation(res_dict)
+
+
     except:
         logger.error("could not create " + label + " reservations")
 
@@ -36,32 +51,32 @@ def execute(directory, allrack=None, allswitch=None,
     if not (allrack or allswitch or bandwidth or nodes or allpair):
     # if not args.allrack and not args.allswitch and not args.bandwidth and not args.nodes and not args.allpair:
 
-         create_reservation( "list_all_rack_bad_not_tested_nodes", "benchmark-alltoall-rack",directory)
-         create_reservation( "list_all_switch_bad_not_tested_nodes", "benchmark-alltoall-switch",directory)
-         create_reservation( "list_all_pair_bad_not_tested_nodes", "benchmark-alltoall-pair",directory)
-         create_reservation( "list_band_bad_not_tested_nodes", "benchmark-bandwidth",directory)
-         create_reservation( "list_node_bad_not_tested_nodes", "benchmark-node",directory)
+         create_reservation( "list_all_rack_bad_not_tested_nodes", "benchmark-alltoall-rack", directory)
+         create_reservation( "list_all_switch_bad_not_tested_nodes", "benchmark-alltoall-switch", directory)
+         create_reservation( "list_all_pair_bad_not_tested_nodes", "benchmark-alltoall-pair", directory)
+         create_reservation( "list_band_bad_not_tested_nodes", "benchmark-bandwidth", directory)
+         create_reservation( "list_node_bad_not_tested_nodes", "benchmark-node", directory)
          # create_reservation( "not_tested", "benchmark-not-tested",directory)
-         create_reservation( "not_in_test", "benchmark-not-in-test",directory)
+         create_reservation( "not_in_test", "benchmark-not-in-test", directory)
 
     else:
 
         print args
 
         if allrack==True:
-           create_reservation( "list_all_rack_bad_not_tested_nodes", "benchmark_alltoall_rack",directory)
+           create_reservation( "list_all_rack_bad_not_tested_nodes", "benchmark_alltoall_rack", directory)
 
         if allswitch==True:
-           create_reservation( "list_all_switch_bad_not_tested_nodes", "benchmark_alltoall_switch",directory)
+           create_reservation( "list_all_switch_bad_not_tested_nodes", "benchmark_alltoall_switch", directory)
 
         if allpair==True:
-           create_reservation( "list_all_pair_bad_not_tested_nodes", "benchmark_alltoall_pair",directory)
+           create_reservation( "list_all_pair_bad_not_tested_nodes", "benchmark_alltoall_pair", directory)
 
         if bandwidth==True:
-            create_reservation( "list_band", "benchmark_bandwidth",directory)
+            create_reservation( "list_band", "benchmark_bandwidth", directory)
 
         if nodes==True:
-            create_reservation( "list_node", "benchmark_node",directory)
+            create_reservation( "list_node", "benchmark_node", directory)
             #create_reservation( "not_tested", "benchmark-not-tested",directory)
             #create_reservation( "not_in_test", "benchmark-not-in-test",directory)
 
