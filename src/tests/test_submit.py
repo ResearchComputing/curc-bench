062695f5e5f21db0827c5e78a550eb10f21f5f6f
Jonathon Anderson <jonathon.anderson@colorado.edu>
2015-04-10 11:39:44 -0600

    Clean up test_submit
    Mostly tabs-to-spaces conversion.
    
    https://www.python.org/dev/peps/pep-0008/#indentation
    
    Also removed __main__ block at the bottom. (Tests should be
    run by setup.py or nosetests.)

--- src/tests/test_submit.py
+++ src/tests/test_submit.py
@@ -4,7 +4,6 @@ import os
 import shutil
 import tempfile
 import unittest
-from testfixtures import TempDirectory
 
 
 def fake_subprocess(cmd, shell):
@@ -12,56 +11,54 @@ def fake_subprocess(cmd, shell):
     subprocess.check_output = mock.Mock(return_value=([cmd], shell))
     return subprocess
 
+
 class TestSubmit(unittest.TestCase):
 
-	def setUp (self):
-		self.directory = tempfile.mkdtemp()
-		self.folder = tempfile.mkdtemp(dir=self.directory)
-		self.file = tempfile.NamedTemporaryFile(prefix='script_10', dir=self.folder)
+    def setUp (self):
+        self.directory = tempfile.mkdtemp()
+        self.folder = tempfile.mkdtemp(dir=self.directory)
+        self.file = tempfile.NamedTemporaryFile(prefix='script_10', dir=self.folder)
 
-	def tearDown (self):
-		shutil.rmtree(self.folder)
-		shutil.rmtree(self.directory)
+    def tearDown (self):
+        shutil.rmtree(self.folder)
+        shutil.rmtree(self.directory)
 
-	@mock.patch('bench.submit.subprocess.check_output',
-				return_value=fake_subprocess('script_10', True))
-	def test_submit(self, arg1):
-		index = 0
-		pause = None
-		reservation = 'PM'
-		qos = "High"
-		account = "Account"
-		new_index = bench.submit.submit(self.directory, self.folder,
-							 			index, pause, reservation, qos, account)
+    @mock.patch('bench.submit.subprocess.check_output',
+                return_value=fake_subprocess('script_10', True))
+    def test_submit(self, arg1):
+        index = 0
+        pause = None
+        reservation = 'PM'
+        qos = "High"
+        account = "Account"
+        new_index = bench.submit.submit(self.directory, self.folder,
+                                        index, pause, reservation, qos, account)
 
-		#Did it find the file?
-		self.assertEqual(1, new_index)
-		#Arguments correct?
-		self.assertTrue('sbatch ' in str(arg1.call_args_list))
-		self.assertTrue(' script_10' in str(arg1.call_args_list))
-		self.assertTrue(' --reservation=PM ' in str(arg1.call_args_list))
-		self.assertTrue(' --qos=High ' in str(arg1.call_args_list))
-		self.assertTrue(' --account=Account ' in str(arg1.call_args_list))
-		#Shell==True?
-		self.assertEqual(True, arg1.call_args_list[0][1]['shell'])
+        #Did it find the file?
+        self.assertEqual(1, new_index)
+        #Arguments correct?
+        self.assertTrue('sbatch ' in str(arg1.call_args_list))
+        self.assertTrue(' script_10' in str(arg1.call_args_list))
+        self.assertTrue(' --reservation=PM ' in str(arg1.call_args_list))
+        self.assertTrue(' --qos=High ' in str(arg1.call_args_list))
+        self.assertTrue(' --account=Account ' in str(arg1.call_args_list))
+        #Shell==True?
+        self.assertEqual(True, arg1.call_args_list[0][1]['shell'])
 
-	@mock.patch('bench.submit.subprocess.check_output',
-				return_value=fake_subprocess('script_10', True))
-	def test_submit2(self, arg1):
-		index = 0
-		pause = None
-		reservation = None
-		qos = None
-		account = None
-		new_index = bench.submit.submit(self.directory, self.folder,
-							 			index, pause, reservation, qos, account)
+    @mock.patch('bench.submit.subprocess.check_output',
+                return_value=fake_subprocess('script_10', True))
+    def test_submit2(self, arg1):
+        index = 0
+        pause = None
+        reservation = None
+        qos = None
+        account = None
+        new_index = bench.submit.submit(self.directory, self.folder,
+                                        index, pause, reservation, qos, account)
 
-		#Did it find the file?
-		self.assertEqual(1, new_index)
-		#Arguments correct?
-		self.assertTrue('sbatch script_10' in str(arg1.call_args_list))
-		#Shell==True?
-		self.assertEqual(True, arg1.call_args_list[0][1]['shell'])
-
-if __name__ == '__main__':
-	unittest.main()
+        #Did it find the file?
+        self.assertEqual(1, new_index)
+        #Arguments correct?
+        self.assertTrue('sbatch script_10' in str(arg1.call_args_list))
+        #Shell==True?
+        self.assertEqual(True, arg1.call_args_list[0][1]['shell'])
