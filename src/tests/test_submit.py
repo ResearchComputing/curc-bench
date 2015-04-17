1a2e6a0e2817654fd4d13c8f8bc1dc40a2d7f2dc
AaronTHolt <holtat@colorado.edu>
2015-04-17 11:01:07 -0600

    Update submit.py tests

--- src/tests/test_submit.py
+++ src/tests/test_submit.py
@@ -17,14 +17,14 @@ class TestSubmit(unittest.TestCase):
     def setUp (self):
         self.directory = tempfile.mkdtemp()
         self.folder = tempfile.mkdtemp(dir=self.directory)
-        self.file = tempfile.NamedTemporaryFile(prefix='script_10', dir=self.folder)
+        self.file = tempfile.NamedTemporaryFile(prefix='node0442', dir=self.folder)
 
     def tearDown (self):
         shutil.rmtree(self.folder)
         shutil.rmtree(self.directory)
 
     @mock.patch('bench.submit.subprocess.check_output',
-                return_value=fake_subprocess('script_10', True))
+                return_value=fake_subprocess('node0442', True))
     def test_submit(self, arg1):
         index = 0
         pause = None
@@ -38,7 +38,7 @@ class TestSubmit(unittest.TestCase):
         self.assertEqual(1, new_index)
         #Arguments correct?
         self.assertTrue('sbatch ' in str(arg1.call_args_list))
-        self.assertTrue(' script_10' in str(arg1.call_args_list))
+        self.assertTrue(' node0442' in str(arg1.call_args_list))
         self.assertTrue(' --reservation=PM ' in str(arg1.call_args_list))
         self.assertTrue(' --qos=High ' in str(arg1.call_args_list))
         self.assertTrue(' --account=Account ' in str(arg1.call_args_list))
@@ -46,7 +46,7 @@ class TestSubmit(unittest.TestCase):
         self.assertEqual(True, arg1.call_args_list[0][1]['shell'])
 
     @mock.patch('bench.submit.subprocess.check_output',
-                return_value=fake_subprocess('script_10', True))
+                return_value=fake_subprocess('node0442', True))
     def test_submit2(self, arg1):
         index = 0
         pause = None
@@ -59,6 +59,6 @@ class TestSubmit(unittest.TestCase):
         #Did it find the file?
         self.assertEqual(1, new_index)
         #Arguments correct?
-        self.assertTrue('sbatch script_10' in str(arg1.call_args_list))
+        self.assertTrue('sbatch node0442' in str(arg1.call_args_list))
         #Shell==True?
         self.assertEqual(True, arg1.call_args_list[0][1]['shell'])
