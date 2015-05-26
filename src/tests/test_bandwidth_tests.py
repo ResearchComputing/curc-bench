e54926ea3d0cb5c6c40f3b3c9b3815ccb0366713
Jonathon Anderson <jonathon.anderson@colorado.edu>
2015-05-26 12:21:56 -0600

    Commit missing bandwidth tests

---
+++ src/tests/test_bandwidth_tests.py
@@ -0,0 +1,37 @@
+import bench.tests.bandwidth
+import os
+import shutil
+import tempfile
+import unittest
+
+
+class TestBandwidthProcess (unittest.TestCase):
+
+    def setUp (self):
+        self.prefix = tempfile.mkdtemp()
+        self.test_dir = tempfile.mkdtemp(dir=self.prefix)
+        self.osu_bw = os.path.join(self.test_dir, 'osu_bw.out')
+        with open(self.osu_bw, 'w') as fp:
+            pass # just create the file
+        assert os.path.exists(self.osu_bw)
+        self.node_list = os.path.join(self.test_dir, 'node_list')
+        with open(self.node_list, 'w') as fp:
+            fp.write('tnode1\n')
+
+    def tearDown (self):
+        if os.path.exists(self.prefix):
+            shutil.rmtree(self.prefix)
+
+    def test_empty (self):
+        result = bench.tests.bandwidth.process([], self.prefix)
+        self.assertEqual(result['good_nodes'], set())
+        self.assertEqual(result['bad_nodes'], set(['tnode1']))
+        self.assertEqual(result['not_tested'], set())
+
+    def test_garbage (self):
+        with open(self.osu_bw, 'w') as fp:
+            fp.write('asfd\nhjkl\n')
+        result = bench.tests.bandwidth.process([], self.prefix)
+        self.assertEqual(result['good_nodes'], set())
+        self.assertEqual(result['bad_nodes'], set(['tnode1']))
+        self.assertEqual(result['not_tested'], set())
