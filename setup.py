02317a63b1a8cfac478c97a9a84bea100e4064ed
Jonathon Anderson <jonathon.anderson@colorado.edu>
2015-11-12 22:27:02 -0700

    Use git-describe in setup.py for version

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ use_setuptools()
 import multiprocessing
 import os
 import setuptools
+import subprocess
+
 
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
@@ -12,30 +14,42 @@ import setuptools
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-setuptools.setup(
-    name = "bench",
-    version = "0.2.0",
-    author = "Research Computing",
-    author_email = "",
-    description = ("HPC Benchmarking"),
-    license = "BSD",
-    keywords = "",
-    url = "",
-    package_dir = {'': 'src'},
-    packages=['bench'],
-    install_requires=['argparse', 'jinja2', 'python-hostlist'],#, 'NumPy>=1.7.0'],
-    tests_require=['nose', 'importlib', 'mock'],
-    test_suite = 'nose.collector',
-    long_description=read('README.mdwn'),
-    classifiers=[
-        "License :: BSD License",
-    ],
-    entry_points={
-        'console_scripts': [
-            'bench = bench.driver:driver',
+
+def git_describe():
+    git_describe = subprocess.Popen(['git', 'describe'], stdout=subprocess.PIPE)
+    stdout, _ = git_describe.communicate()
+    return stdout.strip()
+
+
+def main ():
+    setuptools.setup(
+        name = 'bench',
+        version = git_describe(),
+        author = 'Research Computing',
+        author_email = '',
+        description = ('HPC Benchmarking'),
+        license = 'BSD',
+        keywords = '',
+        url = '',
+        package_dir = {'': 'src'},
+        packages=['bench'],
+        install_requires=['argparse', 'jinja2', 'python-hostlist'],#, 'NumPy>=1.7.0'],
+        tests_require=['nose', 'importlib', 'mock'],
+        test_suite = 'nose.collector',
+        long_description=read('README.mdwn'),
+        classifiers=[
+            'License :: BSD License',
         ],
-    },
-    package_data={
-        'bench.tests': ['*.job'],
-    },
-)
+        entry_points={
+            'console_scripts': [
+                'bench = bench.driver:driver',
+            ],
+        },
+        package_data={
+            'bench.tests': ['*.job'],
+        },
+    )
+
+
+if __name__ == '__main__':
+    main()
