fdb85edf97819b1a9c2c8b2652d985ea15eb53fb
AaronTHolt <holtat@colorado.edu>
2017-03-10 14:55:14 -0700

    Add command 'add2' to driver. Instantiate a TestFramework class. 'add2' should do the exact same thing as 'add' command except use the new test plugin framework. The idea is to remove add.py after all the code is moved into the new plugin system.

--- src/bench/driver.py
+++ src/bench/driver.py
@@ -1,6 +1,8 @@
 import argparse
 import bench.add
 import bench.create
+import bench.framework
+import bench.framework_add
 import bench.log
 import bench.process
 import bench.reserve
@@ -14,7 +16,6 @@ import sys
 
 
 logger = logging.getLogger(__name__)
-
 
 def parser(*args, **kwargs):
     parser = argparse.ArgumentParser(*args, **kwargs)
@@ -34,6 +35,12 @@ def parser(*args, **kwargs):
     add.add_argument('-t', '--topology-file',
                      help='slurm topology.conf')
     parser_add_filter_arguments(add)
+
+    add2 = subparsers.add_parser('add2', help='add tests for submission')
+    parser_add_test_arguments(add2)
+    add2.add_argument('-t', '--topology-file',
+                     help='slurm topology.conf')
+    parser_add_filter_arguments(add2)
 
     submit = subparsers.add_parser('submit', help='submit jobs for scheduling')
     parser_add_test_arguments(submit)
@@ -179,6 +186,8 @@ def driver(argv=None):
 
     bench.log.configure_file_logging(directory)
 
+    frmwk = bench.framework.TestFramework()
+
     if args.command == 'create':
         bench.create.execute(
             directory,
@@ -194,6 +203,24 @@ def driver(argv=None):
 
     elif args.command == 'add':
         bench.add.execute(
+            directory, args.topology_file or os.environ.get('BENCH_TOPOLOGY'),
+            alltoall_rack_tests = args.alltoall_rack_tests,
+            alltoall_switch_tests = args.alltoall_switch_tests,
+            alltoall_pair_tests = args.alltoall_pair_tests,
+            bandwidth_tests = args.bandwidth_tests,
+            node_tests = args.node_tests,
+            include_nodes = args.include_nodes,
+            exclude_nodes = args.exclude_nodes,
+            include_reservations = args.include_reservations,
+            exclude_reservations = args.exclude_reservations,
+            include_states = args.include_states,
+            exclude_states = args.exclude_states,
+            include_files = args.include_files,
+            exclude_files = args.exclude_files,
+        )
+
+    elif args.command == 'add2':
+        frmwk.Add.execute(
             directory, args.topology_file or os.environ.get('BENCH_TOPOLOGY'),
             alltoall_rack_tests = args.alltoall_rack_tests,
             alltoall_switch_tests = args.alltoall_switch_tests,
