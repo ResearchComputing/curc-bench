401c23afbcd24ec3b9f4b52e323280c712e44db1
AaronTHolt <holtat@colorado.edu>
2017-03-14 13:57:13 -0600

    Consolidate the add portion of the bandwidth test into a plugin class.

---
+++ src/bench/tests/bandwidth_test.py
@@ -0,0 +1,37 @@
+import bench.framework
+import bench.exc
+import bench.util
+import jinja2
+import logging
+import os
+import pkg_resources
+import re
+
+
+class BandwidthTest(bench.framework.TestFramework):
+
+    def __init__(self):
+        bench.framework.TestFramework.__init__(self)
+
+        self.Add = bench.framework_add.Add(self.logger, self.generate)
+
+        self.TEMPLATE = jinja2.Template(
+            pkg_resources.resource_string(__name__, 'bandwidth.job'),
+            keep_trailing_newline=True,
+        )
+
+    def generate(self, nodes, prefix, topology=None, test_name=None):
+        if not topology:
+            topology = {}
+        node_pairs = bench.infiniband.get_switch_node_pairs(nodes, topology)
+        for pair_name, node_pair in node_pairs.iteritems():
+            test_dir = os.path.join(prefix, pair_name)
+            bench.util.mkdir_p(test_dir)
+            script = os.path.join(test_dir, '{0}.job'.format(pair_name))
+            with open(script, 'w') as fp:
+                fp.write(self.TEMPLATE.render(
+                    job_name = 'bench-bandwidth-{0}'.format(pair_name),
+                    nodes = node_pair,
+                ))
+            bench.util.write_node_list(os.path.join(test_dir, 'node_list'), node_pair)
+        self.logger.info('bandwidth: add: {0}'.format(len(node_pairs)))
