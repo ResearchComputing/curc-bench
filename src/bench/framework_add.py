cff087b6639ef4243b0a04f685dd931106adb401
AaronTHolt <holtat@colorado.edu>
2017-05-22 14:49:40 -0600

    Remove another topology file referece and unused function inputs

--- src/bench/framework_add.py
+++ src/bench/framework_add.py
@@ -11,12 +11,7 @@ class Add(object):
         self.include_states = None
         self.exclude_states = None
 
-    def execute(self, prefix, topology_file=None,
-              alltoall_rack_tests=None,
-              alltoall_switch_tests=None,
-              alltoall_pair_tests=None,
-              bandwidth_tests=None,
-              node_tests=None,
+    def execute(self, prefix,
               include_states=None,
               exclude_states=None,
               **kwargs
@@ -30,11 +25,6 @@ class Add(object):
                                               exclude_states=exclude_states,
                                               **kwargs)
 
-        if topology_file is not None:
-            topology = bench.infiniband.get_topology(topology_file)
-        else:
-            topology = {}
-
         #make the test directory and write the node list to it
         self.test_prefix = os.path.join(prefix, self.test_name)
         bench.util.mkdir_p(os.path.join(self.test_prefix))
@@ -42,4 +32,4 @@ class Add(object):
             os.path.join(self.test_prefix, 'node_list'),
             node_list)
 
-        self.add_tests(node_list, self.test_prefix, topology)
+        self.add_tests(node_list, self.test_prefix)
