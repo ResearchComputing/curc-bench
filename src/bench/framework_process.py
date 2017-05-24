66afc95dbee344186381e73f8cac657a8884ce2a
AaronTHolt <holtat@colorado.edu>
2017-05-24 16:02:41 -0600

    Generalize process function in framework_process -Now loops through every subtest rather than needing an large if statement. For example, the 'node' test has linpack and stream subtests which are both run. parse_data and evaluate_data are called for each subtest rather than parse_stream, evaluate stream, parse_linpack, and evaluate_linpack. This cuts down on code volume and should make adding subtests easier.

--- src/bench/framework_process.py
+++ src/bench/framework_process.py
@@ -1,25 +1,26 @@
 import bench.util
-import bench.tests.node
-import bench.tests.bandwidth
-import bench.tests.alltoall
+import bench.tests.node_test
+# import bench.tests.bandwidth
+# import bench.tests.alltoall
 import hostlist
 import logging
 import os
 
 class Process(object):
-    
-    def __init__(self, logger, generate, test_name):
+
+    def __init__(self, logger, parse_data, evaluate_data, test_name, subtests):
         self.logger = logger
         self.test_name = test_name
-        self.PROCESSORS = {
-            'node': bench.tests.node.process,
-            'bandwidth': bench.tests.bandwidth.process,
-            'alltoall-rack': bench.tests.alltoall.process,
-            'alltoall-switch': bench.tests.alltoall.process,
-            'alltoall-pair': bench.tests.alltoall.process,
-        }
-        
-    def write_result_files(prefix, pass_nodes, fail_nodes, error_nodes):
+        self.parse_data = parse_data
+        self.evaluate_data = evaluate_data
+        self.subtests = subtests
+
+
+    def execute(self, prefix):
+        self.test_prefix = os.path.join(prefix, self.test_name)
+        self.process_tests(self.test_prefix)
+
+    def write_result_files(self, prefix, pass_nodes, fail_nodes, error_nodes):
         bench.util.write_node_list(
             os.path.join(prefix, 'pass_nodes'),
             pass_nodes,
@@ -34,53 +35,74 @@ class Process(object):
             os.path.join(prefix, 'error_nodes'),
             error_nodes,
         )
-        
-    def process_tests (prefix, key):
-        prefix_ = os.path.join(prefix, key, 'tests')
+
+    def process_tests (self, prefix):
+        prefix_ = os.path.join(prefix, 'tests')
+        print("Prefix_, prefix = ", prefix_, prefix)
         if not os.path.exists(prefix_):
-            self.logger.warn('{0}: not found'.format(key))
+            self.logger.warn('{0}: not found'.format(prefix))
             return
-        node_list = bench.util.read_node_list(os.path.join(prefix, key, 'node_list'))
-        results = PROCESSORS[key](node_list, prefix_)
+
+        node_list = bench.util.read_node_list(os.path.join(prefix, 'node_list'))
+        fail_nodes = set()
+        pass_nodes = set()
+
+        for test in os.listdir(prefix_):
+            # print("prefix, test = ", prefix, test)
+            test_dir = os.path.join(prefix_, test)
+            test_nodes = set(bench.util.read_node_list(os.path.join(test_dir, 'node_list')))
+
+            for subtest in self.subtests:
+                path = os.path.join(test_dir, subtest+'.out')
+                self.process(path, test, subtest, fail_nodes, pass_nodes, test_nodes)
+
+        tested = pass_nodes | fail_nodes
+        error_nodes = set(node_list) - tested
+
         self.logger.info('{0}: fail nodes: {1} / {2}'.format(
-            key, len(results['fail_nodes']), len(node_list)))
+            prefix, len(fail_nodes), len(node_list)))
         self.logger.info('{0}: pass nodes: {1} / {2}'.format(
-            key, len(results['pass_nodes']), len(node_list)))
+            prefix, len(pass_nodes), len(node_list)))
         self.logger.info('{0}: error nodes: {1} / {2}'.format(
-            key, len(results['error_nodes']), len(node_list)))
+            prefix, len(error_nodes), len(node_list)))
         self.write_result_files(
-            os.path.join(prefix, key),
-            results['pass_nodes'],
-            results['fail_nodes'],
-            results['error_nodes'],
-        )
-        
-    def execute(self, prefix,alltoall_rack_tests=None,
-                alltoall_switch_tests=None,
-                alltoall_pair_tests=None,
-                bandwidth_tests=None,
-                node_tests=None):
-                
-        process_any_tests_explicitly = (
-            alltoall_rack_tests
-            or alltoall_switch_tests
-            or alltoall_pair_tests
-            or bandwidth_tests
-            or node_tests
+            prefix,
+            pass_nodes,
+            fail_nodes,
+            error_nodes,
         )
 
-        # default to processing *all* test results
-        if not process_any_tests_explicitly:
-            for key in self.PROCESSORS:
-                self.process_tests(prefix, key)
+    def process(self, path, test, subtest, fail_nodes, pass_nodes, test_nodes):
+        ''' Inputs
+        test - directory containing test results, ex: node001, rack4
+        subtest - name of subtest, curc-bench looks for a file called {subtest_name}.out
+        (some tests contain multiple subtests, for example, a "node test" might run
+        both stream and linpack.)
+        '''
+        try:
+            with open(path) as fp:
+                output = fp.read()
+            data = self.parse_data(output, subtest)
+        except IOError as ex:
+            self.logger.warn('{0}: error (unable to read {1})'.format(test, path))
+            self.logger.debug(ex, exc_info=True)
+            return
+        except bench.exc.ParseError as ex:
+            self.logger.warn('{0}: error (unable to parse {1})'.format(test, path))
+            self.logger.debug(ex, exc_info=True)
+            return
+        passed = self.evaluate_data(data, subtest)
+
+        if passed:
+            self.logger.info('{0}: pass'.format(test))
+            pass_nodes |= test_nodes
         else:
-            if alltoall_rack_tests:
-                self.process_tests(prefix, 'alltoall-rack')
-            if alltoall_switch_tests:
-                self.process_tests(prefix, 'alltoall-switch')
-            if alltoall_pair_tests:
-                self.process_tests(prefix, 'alltoall-pair')
-            if bandwidth_tests:
-                self.process_tests(prefix, 'bandwidth')
-            if node_tests:
-                self.process_tests(prefix, 'node')
+            fail_nodes |= test_nodes
+            self.logger.info('{test}: fail {subtest}'.format(test=test,
+                                                            subtest=subtest))
+        return
+
+
+
+
+        #
