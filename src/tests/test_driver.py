5fdf605c4f1c82a1866c0432f97317da0182eeb0
AaronTHolt <holtat@colorado.edu>
2017-07-14 15:04:16 -0600

    Update test_driver to use new plugin framework.

--- src/tests/test_driver.py
+++ src/tests/test_driver.py
@@ -2,6 +2,8 @@ import bench.driver
 import argparse
 import mock
 import unittest
+from bench.tests.node_test import NodeTest
+from bench.framework_submit import Submit
 
 class TestDriver(unittest.TestCase):
 
@@ -13,9 +15,9 @@ class TestDriver(unittest.TestCase):
     def test_parser_add(self, *arg):
         '''Test that arguments are being parsed as expected'''
         parsed = self.parser.parse_args(['-d', 'fake_dir',
-                                '-v', 'add', '--node-tests'])
+                                '-v', 'add', '--test', 'node'])
         self.assertEqual(parsed.command, 'add')
-        self.assertEqual(parsed.node_tests, True)
+        self.assertEqual(parsed.test, 'node')
         self.assertEqual(parsed.verbose, True)
         self.assertEqual(parsed.directory, 'fake_dir')
 
@@ -25,73 +27,18 @@ class TestDriver(unittest.TestCase):
         return_value=None)
     @mock.patch('bench.log.configure_file_logging',
         return_value=None)
-    @mock.patch('bench.add.execute',
+    @mock.patch('bench.framework_submit.Submit.execute',
         return_value=None)
-    def test_call_bench_add_execute_1(self, *arg):
-        '''Test that bench.add.execute is being called correctly WITH
-        a specified topology file'''
-        bench.driver.driver(argv=['bench', 'add', '--bandwidth-tests', 
-            '-t', 'fake_topology_file'])
-        assert bench.add.execute.called
-        assert 'fake_dir' in bench.add.execute.call_args[0]
-        assert 'fake_topology_file' in bench.add.execute.call_args[0]
-        assert bench.add.execute.call_args[1]['bandwidth_tests']
-
-    @mock.patch('bench.driver.get_directory',
-        return_value='fake_dir')
-    @mock.patch('bench.log.configure_stderr_logging',
-        return_value=None)
-    @mock.patch('bench.log.configure_file_logging',
-        return_value=None)
-    @mock.patch('bench.add.execute',
-        return_value=None)
-    @mock.patch('os.environ.get',
-        return_value='fake_topology_file')
-    def test_call_bench_add_execute_2(self, *arg):
-        '''Test that bench.add.execute is being called correctly WITHOUT
-        a specified topology file'''
-        bench.driver.driver(argv=['bench', 'add', '--bandwidth-tests'])
-        assert bench.add.execute.called
-        assert 'fake_dir' in bench.add.execute.call_args[0]
-        assert 'fake_topology_file' in bench.add.execute.call_args[0]
-        assert bench.add.execute.call_args[1]['bandwidth_tests']
-
-
-    @mock.patch('bench.driver.get_directory',
-        return_value='fake_dir')
-    @mock.patch('bench.log.configure_stderr_logging',
-        return_value=None)
-    @mock.patch('bench.log.configure_file_logging',
-        return_value=None)
-    @mock.patch('bench.submit.execute',
-        return_value=None)
+    # @mock.patch('bench.tests.node_test.NodeTest',
+    #     return_value=None)
     def test_call_bench_submit_execute_1(self, *arg):
-        '''Test that bench.submit.execute is being called correctly WITH
+        '''Test that bench.tests.node_test.NodeTest Submit.execute is being called correctly WITH
         a specified reservation'''
-        bench.driver.driver(argv=['bench', 'submit', '--node-tests', 
+        bench.driver.driver(argv=['bench', 'submit', '--test', 'node',
             '--reservation', 'fake_reservation'])
-        assert bench.submit.execute.called
-        assert 'fake_dir' in bench.submit.execute.call_args[0]
-        assert bench.submit.execute.call_args[1]['node_tests']
-        self.assertEqual(bench.submit.execute.call_args[1]['reservation'], 
-            'fake_reservation')
-
-    @mock.patch('bench.driver.get_directory',
-        return_value='fake_dir')
-    @mock.patch('bench.log.configure_stderr_logging',
-        return_value=None)
-    @mock.patch('bench.log.configure_file_logging',
-        return_value=None)
-    @mock.patch('bench.submit.execute',
-        return_value=None)
-    @mock.patch('os.environ.get',
-        return_value='fake_reservation')
-    def test_call_bench_submit_execute_2(self, *arg):
-        '''Test that bench.submit.execute is being called correctly WITHOUT
-        a specified reservation'''
-        bench.driver.driver(argv=['bench', 'submit', '--node-tests'])
-        assert bench.submit.execute.called
-        assert 'fake_dir' in bench.submit.execute.call_args[0]
-        assert bench.submit.execute.call_args[1]['node_tests']
-        self.assertEqual(bench.submit.execute.call_args[1]['reservation'], 
-            'fake_reservation')
+        print("Submit args", Submit.execute.call_args)
+        assert Submit.execute.called
+        assert 'fake_dir' in Submit.execute.call_args[0]
+        # assert Submit.execute.call_args[1]['--test']
+        self.assertEqual(Submit.execute.call_args[1]['reservation'],
+             'fake_reservation')
