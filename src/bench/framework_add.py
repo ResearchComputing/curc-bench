d96672635f4e5348219439386144795bff9c2ffb
AaronTHolt <holtat@colorado.edu>
2018-01-23 13:38:35 -0700

    Add ior test -Modify framework_add to allow for tests without explicit nodelist -The ior test currently doesn't have 'process' or 'reserve' working. 'reserve' won't be implemented because ior tests hardware that isn't reservable by slurm.

--- src/bench/framework_add.py
+++ src/bench/framework_add.py
@@ -21,31 +21,38 @@ class Add(object):
         if not (include_states or exclude_states):
           exclude_states = ['down', 'draining', 'drained']
 
-        test_node_list = set(hostlist.expand_hostlist(bc.config[self.test_name]["nodes"]))
 
-        #Exclude curc-bench made reservations (not including current test)
-        curcb_res = []
-        curcb_res_nodes = set()
-        all_res_data = bench.slurm.scontrol('show', 'res')
-        all_res_data = all_res_data.split('\n\n')
+        node_list = set()
 
-        for res in all_res_data:
-            if 'test_name' in res:
-                continue
-            if 'bench-' in res :
-                curcb_res.append(res.split(' ')[0].split('=')[1]) # Add reservation name to list
+        # Try/except for tests with an explicit node list vs tests without
+        try:
+            test_node_list = set(hostlist.expand_hostlist(bc.config[self.test_name]["nodes"]))
 
-        for res in curcb_res:
-            curcb_res_nodes |= bench.util.get_reserved_nodes(res)
+            #Exclude curc-bench made reservations (not including current test)
+            curcb_res = []
+            curcb_res_nodes = set()
+            all_res_data = bench.slurm.scontrol('show', 'res')
+            all_res_data = all_res_data.split('\n\n')
 
-        test_node_list -= curcb_res_nodes
+            for res in all_res_data:
+                if 'test_name' in res:
+                    continue
+                if 'bench-' in res :
+                    curcb_res.append(res.split(' ')[0].split('=')[1]) # Add reservation name to list
 
-        # Manual, command line filtering
-        # Includes/excludes here override curc-bench reservation excludes
-        node_list = bench.util.filter_node_list(test_node_list,
-                                              include_states=include_states,
-                                              exclude_states=exclude_states,
-                                              **kwargs)
+            for res in curcb_res:
+                curcb_res_nodes |= bench.util.get_reserved_nodes(res)
+
+            test_node_list -= curcb_res_nodes
+
+            # Manual, command line filtering
+            # Includes/excludes here override curc-bench reservation excludes
+            node_list = bench.util.filter_node_list(test_node_list,
+                                                  include_states=include_states,
+                                                  exclude_states=exclude_states,
+                                                  **kwargs)
+        except KeyError:
+            pass                          
 
         #make the test directory and write the node list to it
         self.test_prefix = os.path.join(prefix, self.test_name)
---
+++ src/bench/tests/ior.py
@@ -0,0 +1,58 @@
+import bench.framework
+import bench.exc
+import bench.util
+import bench.configuration as bc
+import jinja2
+import logging
+import os
+import pkg_resources
+import re
+import collections
+import hostlist
+
+
+class IorTest(bench.framework.TestFramework):
+
+    def __init__(self, test_name):
+        bench.framework.TestFramework.__init__(self, test_name)
+
+        self.Add = bench.framework_add.Add(self.logger, self.generate, test_name)
+        self.Submit = bench.framework_submit.Submit(self.logger, test_name)
+        self.Process = bench.framework_process.Process(self.logger, self.parse_data, self.evaluate_data, test_name)
+        self.Reserve = bench.framework_reserve.Reserve(self.logger, test_name)
+
+        self.TEMPLATE = jinja2.Template(
+            pkg_resources.resource_string(__name__, 'ior.job'),
+            keep_trailing_newline=True,
+        )
+
+
+
+    def generate(self, nodes, prefix, topology=None, test_name=None):
+
+        if topology:
+            self.logger.info('node: ignoring topology (not used)')
+
+        test_dir = os.path.join(prefix, "tests/ior")
+        bench.util.mkdir_p(test_dir)
+
+        script_file = os.path.join(test_dir, '{0}.job'.format('ior'))
+        with open(script_file, 'w') as fp:
+            fp.write(self.TEMPLATE.render(
+                job_name = 'bench-ior',
+                ior_path = bc.config['ior']['ior'],
+            ))
+
+        node_list_file = os.path.join(test_dir, 'node_list')
+        bench.util.write_node_list(node_list_file, [''])
+        self.logger.info('ior: adding ior test')
+
+    def parse_data(self, output):
+        return
+
+    def evaluate_data(self, data, *args):
+        return
+
+
+
+#
